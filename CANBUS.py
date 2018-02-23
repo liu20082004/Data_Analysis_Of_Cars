fe0b842be2775d60cca6302dcd2b2036b284d85b
liu2008.2004 <liu2008.2004@163.com>
2018-02-23 17:45:41 +0800

    1.increase common canbus ID process function 2.fix bugs

--- CANBUS.py
+++ CANBUS.py
@@ -12,13 +12,20 @@ class CANBUS_DATA():
 	"""
 
 	def __init__(self, canbus_data, type):
+		self.is_canbus_data = False
 		if "JBT"==type:
-			data = [data for data in canbus_data.strip().split()]
-			self.data = data[1:-1]
-			self.ID = data[0]
-			self.time = data[-1]
-			self.framelen = self.get_framelen()
-			self.keyword = self.get_keyword()
+			data = [data for data in canbus_data.strip().split() if len(canbus_data.strip().split()) > 2]
+			if data:
+				if '.' not in data[-1]:  # 处理最后一帧不带时间参数的细节
+					self.time = None
+					self.data = data[1:]
+				else:
+					self.data = data[1:-1]
+					self.time = data[-1]
+				self.ID = data[0]
+				self.framelen = self.get_framelen()
+				self.keyword = self.get_keyword()
+				self.is_canbus_data = True
 		else:
 			self.data = None
 			self.time = None
@@ -26,15 +33,21 @@ class CANBUS_DATA():
 			self.framelen = None
 			self.keyword = None
 		self.NO = None  # 序列号后续再赋值
+		self.flag = None  # 收发属性,其实相对于总线来说,没有收发的关系,只有是否符合滤波规则这一条
+
+	def set_flag(self, flag):
+		self.flag = flag
 
 	def set_NO(self, NO):
 		self.NO = NO
 
 	def get_framelen(self):
-		if 0x10==(self.data[0]&0x10):
-			framelen = ((self.data[0]&0xf)<<8)|self.data[1]
-		elif (0x20!=(self.data[0]&0x20)) and (0x30!=(self.data[0]&0x30)) and (self.data[0]>0) and (self.data[0]<8):
-			framelen = self.data[0]
+		data1 = int(self.data[0],16)
+		data2 = int(self.data[1],16)
+		if 0x10 == (data1 & 0x10):
+			framelen = ((data1 & 0xf) << 8) | data2
+		elif (0x20 != (data1 & 0x20)) and (0x30 != (data1 & 0x30)) and (data1 > 0) and (data1 < 8):
+			framelen = data1
 		else:
 			framelen = None
 		return framelen
@@ -86,8 +99,45 @@ class CANBUS_RELATIONSHIP():
 		:param canbus_data_list:
 		"""
 
-		self.get_relations_form_file(file_path)
+		self.filter, self.cars, self.common_id = self.get_relations_form_file(file_path)
+		# self.send_id, self.recv_id, self.unknown_id = self.get_relations_of_canbus_data()
 		pass
+
+	def get_relationship_by_known(self, canbus_datas):
+
+		canbus_frist_datas = self.get_all_frist_canbus_data(canbus_datas)
+		frist_data = CANBUS_DATA()
+		for canbus_frist_data in canbus_frist_datas:
+			pass
+
+		pass
+
+	def check_id_by_car_relationship(self, ids):
+		"""此函数用于统计id的命中情况"""
+		send_ids = []
+		recv_ids = []
+
+		for id in ids:
+			for cars in self.cars:
+				for system in cars.systems:
+					pass
+		return send_ids, recv_ids
+
+
+	def get_all_id(self, canbus_datas):
+		ids = []
+		for canbus_data in canbus_datas:
+			if canbus_data.ID not in ids:
+				ids.append(canbus_data.ID)
+		return ids
+
+	def get_all_frist_canbus_data(self, canbus_datas):
+		"""获得起始数据"""
+		canbus_frist_datas = [ canbus_data for canbus_data in canbus_datas if canbus_data.framelen ]
+		return canbus_frist_datas
+
+	def get_relationship_by_filter(self):
+		return self.filter
 
 	def get_relations_form_file(self, file_path):
 		"""
@@ -135,5 +185,43 @@ class CANBUS_RELATIONSHIP():
 
 if __name__ == "__main__":
 	aa = CANBUS_RELATIONSHIP(None, r"D:\JBT\DATA_PRO\3L_CAN\canid_V3.ini")
+
+	filelines = open(r"D:\JBT\DATA_PRO\3L_CAN\untitle.txt", "r").readlines()
+	canbus_datalist = [CANBUS_DATA(eachline, "JBT") for eachline in filelines if CANBUS_DATA(eachline, "JBT").is_canbus_data]
+	canbus_datalist_and_flag = []
+	for each_data in canbus_datalist:
+		if each_data.ID in aa.common_id:
+			each_data.set_flag("Send")
+		else:
+			each_data.set_flag("Unknow")
+		canbus_datalist_and_flag.append(each_data)
+
+	out_data = ''
+	Count = 0
+	for each_data in canbus_datalist_and_flag:
+		if each_data.flag == "Send":
+			if Count < 40:
+				out_data += '\n'
+			Count = 0
+		elif Count < 40:
+			out_data += ' ' * (40 - Count)
+			Count = 40
+		else:
+			out_data += ' ' * 40
+
+		out_data += each_data.ID + ' '
+		Count += 1 + len(each_data.ID)
+		for data in each_data.data:
+			out_data += data + ' '
+		Count += 3 * len(each_data.data)
+
+		if Count > 40:
+			out_data += '\n'
+
+
+
+	outfile = open(r"D:\JBT\DATA_PRO\3L_CAN\OUT.ASM", "w")
+	outfile.write(out_data)
+	outfile.close()
 
 	pass
